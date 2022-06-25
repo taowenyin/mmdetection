bed1c28954eac77ecb74d143275c77da107c34b7
taowenyin <wenyin.tao@163.com>
2022-06-25 10:43:00 +0800

    添加训练配置

--- configs/fire_coco/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_fire_coco.py
+++ configs/fire_coco/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_fire_coco.py
@@ -1,5 +1,6 @@
 _base_ = '../mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
 
+# 定义类别个数
 model = dict(
     roi_head=dict(
         bbox_head=dict(num_classes=1),
@@ -10,20 +11,20 @@ dataset_type = 'CocoDataset'
 classes = ('fire',)
 
 data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=1,
     train=dict(
-        img_prefix='data/fire_coco/train2017/',
+        img_prefix='/mnt/Dataset/Fire_COCO/train2017/',
         classes=classes,
-        ann_file='data/fire_coco/annotations/instances_train2017.json'),
+        ann_file='/mnt/Dataset/Fire_COCO/annotations/instances_train2017.json'),
     val=dict(
-        img_prefix='data/fire_coco/train2017/',
+        img_prefix='/mnt/Dataset/Fire_COCO/val2017/',
         classes=classes,
-        ann_file='data/fire_coco/annotations/instances_val2017.json'),
+        ann_file='/mnt/Dataset/Fire_COCO/annotations/instances_val2017.json'),
     test=dict(
-        img_prefix='data/fire_coco/val2017/',
+        img_prefix='/mnt/Dataset/Fire_COCO/val2017/',
         classes=classes,
-        ann_file='data/fire_coco/annotations/instances_test2017.json'))
+        ann_file='/mnt/Dataset/Fire_COCO/annotations/instances_val2017.json'))
+
+log_config = dict(interval=5)
 
 # 我们可以使用预训练的 Mask R-CNN 来获取更好的性能
 load_from = 'checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
