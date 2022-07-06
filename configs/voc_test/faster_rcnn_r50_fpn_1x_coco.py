63ff4b1912eae1331d638d7d67f5c9d7a3279f18
taowenyin <wenyin.tao@163.com>
2022-07-07 01:00:40 +0800

    不创建超链接

--- configs/voc_test/faster_rcnn_r50_fpn_1x_coco.py
+++ configs/voc_test/faster_rcnn_r50_fpn_1x_coco.py
@@ -36,3 +36,7 @@ data = dict(
         img_prefix=data_root + 'val2017/'))
 
 runner = dict(type='EpochBasedRunner', max_epochs=20)
+
+load_from = '../checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+
+checkpoint_config = dict(create_symlink=False)
