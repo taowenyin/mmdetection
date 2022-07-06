a7ddba26a643c9d3dea3a7624cc2ef0ff6ac69b7
taowenyin <wenyin.tao@163.com>
2022-07-06 18:06:58 +0800

    添加voc数据集的测试配置

---
+++ configs/voc_test/faster_rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,38 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    '../_base_/datasets/coco_detection.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+dataset_type = 'CocoDataset'
+data_root = '/mnt/dataset/voc2012/coco/'
+
+classes = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car',
+           'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train',
+           'tvmonitor')
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=20)))
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        classes=classes,
+        img_prefix=data_root + 'train2017/'),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        classes=classes,
+        img_prefix=data_root + 'val2017/'),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        classes=classes,
+        img_prefix=data_root + 'val2017/'))
+
+runner = dict(type='EpochBasedRunner', max_epochs=20)
