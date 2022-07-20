27b102de4b38ba5b1a665050bd1390cf97320111
taowenyin <wenyin.tao@163.com>
2022-07-20 09:26:30 +0800

    添加FCOS和FasterRCNN模型结果

---
+++ configs/mtfire/faster_rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,45 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    '../_base_/datasets/coco_detection.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+dataset_type = 'CocoDataset'
+
+# Linux
+# data_root = '/home/taowenyin/MyCode/Dataset/fire_coco/'
+# MatPool
+data_root = '/mnt/dataset/fire_coco/'
+# Windows
+# data_root = 'D:/MyCode/Dataset/voc2007/coco/'
+
+classes = ('fire',)
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=1)))
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        classes=classes,
+        img_prefix=data_root + 'train2017/'),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        classes=classes,
+        img_prefix=data_root + 'val2017/'),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        classes=classes,
+        img_prefix=data_root + 'val2017/'))
+
+runner = dict(type='EpochBasedRunner', max_epochs=20)
+
+load_from = './checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+
+checkpoint_config = dict(create_symlink=False)
---
+++ fire_detection/command.md
@@ -0,0 +1,11 @@
+FCOS训练指令
+
+```bash
+python tools/train.py ./configs/mtfire/fcos_r50_caffe_fpn_gn-head_1x_coco.py --work-dir=./fire_detection/
+```
+
+Faster-RCNN训练指令
+
+```bash
+python tools/train.py ./configs/mtfire/faster_rcnn_r50_fpn_1x_coco.py --work-dir=./fire_detection/
+```
